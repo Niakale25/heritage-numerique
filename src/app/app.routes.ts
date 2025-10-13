91ca73fcfde2b0c5391f652b92846da82e8f73d5
niakale <diakitetenin99@gmail.com>
2025-10-13 17:43:42 +0000

    premier commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Accueil } from './pages/accueil/accueil';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Login } from './pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:Accueil,children:[
+        {path:'',component:Dashboard}
+    ]},
+
+        {path:'login',component:Login }
+    
+];
---
+++ src/app/pages/accueil/accueil.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-accueil',
+  imports: [RouterOutlet],
+  templateUrl: './accueil.html',
+  styleUrl: './accueil.css'
+})
+export class Accueil {
+
+}
